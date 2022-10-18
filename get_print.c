424afec8c08b13403b4203563d070f9320a520df
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2022-10-18 23:22:08 +0100

    Team task

--- get_print.c
+++ get_print.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * get_print - selects the right printing function
- * depending on the conversion specifier passed to _printf
- * @s: character that holds the conversion specifier
- * Description: the function loops through the structs array
+ * get_print - selects the right printing function.
+ * depending on the conversion specifier passed to _printf.
+ * @s: character that holds the conversion specifier.
+ * Description: the function loops through the structs array.
  * func_arr[] to find a match between the specifier passed to _printf
- * and the first element of the struct, and then the approriate
- * printing function
- * Return: a pointer to the matching printing function
+ * and the first element of the struct, and then the approriate.
+ * printing function.
+ * Return: a pointer to the matching printing function.
  */
 int (*get_print(char s))(va_list, flags_t *)
 {
