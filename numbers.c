d6ffec95e0c7943f7fc08a253f39e3cbf65b1fb0
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2022-10-19 00:09:59 +0100

    Update numbers.c

--- numbers.c
+++ numbers.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _isdigit - checks if character is digit
- * @c: the character to check
+ * _isdigit - checks if character is digit.
+ * @c: the character to check.
  *
  * Return: 1 if digit, 0 otherwise
  */
