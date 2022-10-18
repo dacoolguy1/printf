830a08fc64e2e3ee9e15095d46df14c80dfe6aed
Gracie1st <112003488+Gracie1st@users.noreply.github.com>
2022-10-18 23:30:49 +0100

    Team Task

--- print_alpha.c
+++ print_alpha.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * print_string - loops through a string and prints
- * every character
- * @l: va_list arguments from _printf
+ * print_string - loops through a string and prints.
+ * every character.
+ * @l: va_list arguments from _printf.
  * @f: pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Return: number of char printed
